44f2b7472bd78345924c12299949a3f75d728a27
Shingala <Vishal.Shingala@Honeywell.com>
2021-09-03 14:41:51 +0530

    updated files add build folder

---
+++ build/precache-manifest.4e474bd3d14283f5f2a8f7c3e0c341d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8ed52c137826bb59045d86d80eafcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00df528fe0f4c21e71a0",
+    "url": "/static/css/main.5f0b32d2.chunk.css"
+  },
+  {
+    "revision": "d3f4a9be486ae96ae5ae",
+    "url": "/static/js/2.40f36e4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40f36e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00df528fe0f4c21e71a0",
+    "url": "/static/js/main.b6a98df4.chunk.js"
+  },
+  {
+    "revision": "edb41b595298e0ff7016",
+    "url": "/static/js/runtime-main.812a00b0.js"
+  }
+]);
